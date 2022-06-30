b8f4227188223af0a8307e3e1963b3628746e1bc
Missy Maloney <missymaloney1@gmail.com>
2022-06-29 21:39:55 -0400

    created categorycard

---
+++ sycXF/Controls/CategoryCard.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace sycXF.Controls
+{
+    public partial class CategoryCard : Grid
+    {
+        public CategoryCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
